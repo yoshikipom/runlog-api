2eddeb36e4a286d1d70b77ed676efe96ed48b954
Yoshiki Shino <yoshiki.shino.tech@gmail.com>
2020-06-07 21:07:34 +0900

    [Add] implement delete

--- src/main/java/jp/yoshikipom/runlogapi/app/controller/RecordController.java
+++ src/main/java/jp/yoshikipom/runlogapi/app/controller/RecordController.java
@@ -9,7 +9,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,6 +42,12 @@ public class RecordController {
     }
     Record record = modelMapper.map(request, Record.class);
     return this.recordService.register(record);
+  }
+
+  @DeleteMapping("/records/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  void deleteRecord(@PathVariable("id") Integer id) {
+    this.recordService.unregister(id);
   }
 
   @GetMapping("/monthRecords")
